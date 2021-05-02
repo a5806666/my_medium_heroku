d1b95c50fc69e26fcbb00c0e082507ab6e1f079d
zhong <afs784512@hotmail.com>
2021-05-02 17:21:15 +0900

    文章狀態管理（狀態機）(aasm_gem_install)

--- app/models/story.rb
+++ app/models/story.rb
@@ -7,4 +7,18 @@ class Story < ApplicationRecord
   def destroy
     update(deleted_at: Time.now)
   end
+
+  include AASM
+  aasm(column: 'status', no_direct_assignment: true ) do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :draft
+    end
+  end
 end
