2a5c4d96a6ae034b3a3acc5e01f895f74ca32265
zhong <afs784512@hotmail.com>
2021-05-05 15:44:31 +0900

    顯示故事列表封面圖

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     def index
         # @stories = Story.all.order(created_at: :desc).includes(:user)
         # @stories = Story.where(status: 'published').order(created_at: :desc).includes(:user)
-        @stories = Story.published.order(created_at: :desc).includes(:user)
+        @stories = Story.published.with_attached_cover_image.order(created_at: :desc).includes(:user)
         @range = 10
 
     end
