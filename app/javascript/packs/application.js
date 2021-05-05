8bf3766482c3a71f3d79ebfe90edabd9ff4fb382
zhong <afs784512@hotmail.com>
2021-05-05 17:42:29 +0900

    heroku scss修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '../stylesheets'
-// import 'bulma'
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
