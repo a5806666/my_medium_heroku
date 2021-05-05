a912340535679891abe9e1ba41054b13b321d76e
zhong <afs784512@hotmail.com>
2021-05-05 11:35:15 +0900

    首頁（故事列表）(root_index.html)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
     
     def index
-        
+        @stories = Story.all.order(created_at: :desc).includes(:user)
+    end
+
+    def show
+        # @story = current_user.stories.find(params[:id])
+    end
+
+    def user
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   
   resources :stories
 
+  # /@user_id/文章標題-123
+  get '@:username/:story_id', to: 'pages#show', as:'story_page'# <-路徑名
+
+  # /@user_id/
+  get '@:username/:story_id', to: 'pages#user', as:'user_page'
+
+  
   root 'pages#index'
 end
